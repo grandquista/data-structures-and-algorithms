198ea46969092dcb8a1610ad6b84fac9bb11f2a0
Adam Grandquist <grandquista@gmail.com>
2018-08-06 15:09:54 -0700

    Refactor binary search tests.

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -1,49 +1,105 @@
-from .binarySearch import binarySearch
+package binarySearch_test
 
+import (
+	"testing"
 
-func TestBinarySearchEmptyArray() {
-    assert binarySearch([], 0) == -1
+	"github.com/grandquista/data-structures-and-algorithms/binary_search"
+)
 
+type lessInt int
 
-func TestBinarySearchFindSingleArray() {
-    assert binarySearch([3], 3) == 0
+func (i lessInt) Less(other binarySearch.Less) bool {
+	switch other := other.(type) {
+	case lessInt:
+		return i < other
+	}
+	return false
+}
 
+func rangeArray(size int) []binarySearch.Less {
+	arr := make([]binarySearch.Less, 0, size)
+	for i := 0; i < size; i++ {
+		arr = append(arr, lessInt(i))
+	}
+	return arr
+}
 
-func TestBinarySearchNotFoundSingleArray() {
-    assert binarySearch([1], 0) == -1
+func lessArray(args ...int) []binarySearch.Less {
+	arr := make([]binarySearch.Less, 0, len(args))
+	for i := range args {
+		arr = append(arr, lessInt(i))
+	}
+	return arr
+}
 
+func TestBinarySearchEmptyArray(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(), lessInt(0)) != -1 {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchNotFoundInShortArray() {
-    assert binarySearch([1, 2, 3], 0) == -1
+func TestBinarySearchFindSingleArray(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(3), lessInt(3)) != 0 {
+		t.Fail()
+	}
+}
 
+func TestBinarySearchNotFoundSingleArray(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(1), lessInt(0)) != -1 {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchFoundAtBegining() {
-    assert binarySearch([0, 1, 2, 3, 4, 5], 0) == 0
+func TestBinarySearchNotFoundInShortArray(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(1, 2, 3), lessInt(0)) != -1 {
+		t.Fail()
+	}
+}
 
+func TestBinarySearchFoundAtBegining(t *testing.T) {
+	if binarySearch.BinarySearch(rangeArray(6), lessInt(0)) != 0 {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchFoundAtEnd() {
-    assert binarySearch([0, 1, 3, 4, 5], 5) == 4
+func TestBinarySearchFoundAtEnd(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(0, 1, 3, 4, 5), lessInt(5)) != 4 {
+		t.Fail()
+	}
+}
 
+func TestBinarySearchFoundAtMiddleEven(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(0, 1, 3, 5), lessInt(3)) != 2 {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchFoundAtMiddleEven() {
-    assert binarySearch([0, 1, 3, 5], 3) == 2
+func TestBinarySearchFoundAtMiddleOdd(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(1, 3, 5), lessInt(3)) != 1 {
+		t.Fail()
+	}
+}
 
+func TestBinarySearchHighValue(t *testing.T) {
+	if binarySearch.BinarySearch(lessArray(1, 3, 5), lessInt(3)) != 1 {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchFoundAtMiddleOdd() {
-    assert binarySearch([1, 3, 5], 3) == 1
+func TestBinarySearchLargeArrayLow(t *testing.T) {
+	if binarySearch.BinarySearch(rangeArray(0xFFFFFF), lessInt(0xFF)) != 0xFF {
+		t.Fail()
+	}
+}
 
+func TestBinarySearchLargeArrayHigh(t *testing.T) {
+	if binarySearch.BinarySearch(rangeArray(0xFFFFFF), lessInt(0xFFFFF)) != 0xFFFFF {
+		t.Fail()
+	}
+}
 
-func TestBinarySearchHighValue() {
-    assert binarySearch([1, 3, 5], 3) == 1
-
-
-func TestBinarySearchLargeArrayLow() {
-    assert binarySearch(list(range(0xFFFFFF)), 0xFF) == 0xFF
-
-
-func TestBinarySearchLargeArrayHigh() {
-    assert binarySearch(list(range(0xFFFFFF)), 0xFFFFF) == 0xFFFFF
-
-
-func TestBinarySearchLargeArrayNotFound() {
-    assert binarySearch(list(range(0xFFFFFF)), -4) == -1
+func TestBinarySearchLargeArrayNotFound(t *testing.T) {
+	if binarySearch.BinarySearch(rangeArray(0xFFFFFF), lessInt(-4)) != -1 {
+		t.Fail()
+	}
+}
