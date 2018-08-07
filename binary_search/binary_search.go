d580b8ff1030836860c1a7c926895029e74ec829
Adam Grandquist <grandquista@gmail.com>
2018-08-07 16:41:36 -0700

    Refactor binary search.

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,13 +1,24 @@
-func binarySearch(array, key) {
-    boundHigh = len(array) - 1
-    boundLow = 0
-    index = 0
-    while boundLow <= boundHigh {
-        index = boundLow + (boundHigh - boundLow) // 2
-        if array[index] == key {
-            return index
-        elif array[index] < key {
-            boundLow = index + 1
-        else {
-            boundHigh = index - 1
-    return -1
+package binarySearch
+
+// Less binary search.
+type Less interface {
+	Less(Less) bool
+}
+
+// BinarySearch binary search.
+func BinarySearch(array []Less, key Less) int {
+	boundHigh := len(array) - 1
+	boundLow := 0
+	for boundLow <= boundHigh {
+		index := boundLow + (boundHigh-boundLow)/2
+		if array[index] == key {
+			return index
+		}
+		if array[index].Less(key) {
+			boundLow = index + 1
+		} else {
+			boundHigh = index - 1
+		}
+	}
+	return -1
+}
